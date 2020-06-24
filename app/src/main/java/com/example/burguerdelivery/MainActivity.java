289c77e62ea060a9a02425dba4f5d40a047fefe6
roban <robantunescampos@gmail.com>
2020-06-24 20:57:30 -0300

    ok

--- app/src/main/java/com/example/burguerdelivery/MainActivity.java
+++ app/src/main/java/com/example/burguerdelivery/MainActivity.java
@@ -8,17 +8,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btncriarconta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Entre ou Cadastre-se");
+        telaCadastro();
     }
 
-    public void telaCadastro(View view){
-        Intent tela = new Intent(MainActivity.this, CadastroActivity.class);
-        startActivity(tela);
+    public void telaCadastro(){
+
+        btncriarconta = (Button) findViewById(R.id.btCriarConta);
+
+        btncriarconta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tela = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivityForResult(tela, 0);
+            }
+        });
     }
 
 }
